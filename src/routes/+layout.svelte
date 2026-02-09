fdf73e3768bace213d4099b397adf559e09f6b40
Gun on Tae-PC <napat.gunt3001@gmail.com>
2026-02-09 19:00:30 +0700

    fix: searchParams not working with server pre-rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import "./layout.css";
 	import favicon from "$lib/assets/favicon.svg";
+	import { onMount } from "svelte";
 
 	let { children } = $props();
 
 	import { page } from "$app/state";
 
-	let forceLightColorSchemeMode = page.url.searchParams.has("forceLightScheme");
+	let forceLightColorSchemeMode = $state(false);
+	// We need to get the searchParams at runtime
+	// otherwise it interferes with static pre-rendering
+	onMount(() => {
+		forceLightColorSchemeMode = page.url.searchParams.has("forceLightScheme");
+	});
 </script>
 
 <svelte:head>
