cca7c136eab82cba1287cefcbed75d09c184fa3c
Gun on Tae-PC <napat.gunt3001@gmail.com>
2026-02-09 18:32:51 +0700

    feat: basic landing page instead of placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Widgets</h1>
+<ul>
+	<li><a href="/relative-time">Relative Time</a></li>
+	<li><a href="/seasons">Seasons</a></li>
+</ul>
