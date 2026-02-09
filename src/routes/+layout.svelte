5cecb0842b48913ebb5638ca24face6e97315a50
Gun on Tae-PC <napat.gunt3001@gmail.com>
2026-02-09 18:54:33 +0700

    feat: add a flag to allow forced light color scheme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
 
 	let { children } = $props();
+
+	import { page } from "$app/state";
+
+	let forceLightColorSchemeMode = page.url.searchParams.has("forceLightScheme");
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	{#if forceLightColorSchemeMode}
+		<meta name="color-scheme" content="light" />
+	{:else}
+		<meta name="color-scheme" content="light dark" />
+	{/if}
+</svelte:head>
 {@render children()}
